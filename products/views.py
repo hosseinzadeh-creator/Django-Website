305c9ab7d1b4266b22193856a9309a0ea7b3b654
hosseinzadeh-creator <mhz562003@gmail.com>
2025-12-23 17:53:11 +0330

    use IF Template Tag in Html
    use IF Template Tag in Html

--- products/views.py
+++ products/views.py
@@ -12,6 +12,7 @@ chocolateDict={
     'dark-chocolate':'Our decadent Dark Deluxe European Compound Coating features a smooth, balanced flavor with rich cocoa notes. It melts quickly into a thin, silky texture with the ease & workability of the finest European chocolates with no tempering required. Eleven o’one Deluxe compounds set with a thin, clean coating that won’t weigh down your treats or hide your details. The deep cocoa flavor pairs beautifully with flavors like orange, espresso, toasted nuts or spiced chili.',
     'colored-white':'Colored Melting Wafers take your desserts to new heights with less effort! Specifically formulated to melt quickly into a smooth, velvet consistency for all of your chocolate-coated treats, our Sweet Shoppe Colored Melting wafers provide a full, even coating with exceptional shine & snap. Not only are they vibrant, our coating wafers are also easy to use - just melt - no tempering required!',
     'flavored-chocolate-wafers':'Our Sweet Shoppe Cream Cheese Melting Wafers add a rich & creamy cream cheese flavor to your treats! Designed to capture the taste of real cream cheese frosting, these wafers provide a rich, creamy flavor & soft finish. Please note these wafers are softer to the touch when compared to our standard formula & may take a bit longer to set, giving you time to perfect your dips, drizzles & molds.',
+    'real-chocolate':None,
 }
 
 def index_view(request):
@@ -66,11 +67,11 @@ def chocolateCat_number_view(request,chocolates):
 
 def chocolateCat_view(request,chocolates):
     chocolateData=chocolateDict.get(chocolates)
-    if chocolateData is not None:
-        context={
+    #if chocolateData is not None:
+    context={
             'chocolateName':chocolates,
             'chocolateDesc':chocolateData
         }#use DTL(Django Template Language) 
-        return render(request,'products/chocolatecat.html',context) #Using the render command to display pages with the DTL method 
+    return render(request,'products/chocolatecat.html',context) #Using the render command to display pages with the DTL method 
         #return HttpResponse(f'<h1> {chocolates}</h1>\n <p>{chocolateData}</p>')
-    return HttpResponseNotFound('<h1>Error 404: page is not found </h1>')
+    #return HttpResponseNotFound('<h1>Error 404: page is not found </h1>')
